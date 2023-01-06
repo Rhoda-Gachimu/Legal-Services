0a57b5d86b5ddd7c7a5de9fb5a40a418ffc1d997
Rhoda Gachimu <gachimurhoda@gmail.com>
2023-01-06 16:20:57 +0300

    LS-21 Components structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import ContactUs from './components/contact-us/ContactUs'
+import Login from './components/login/Login'
+import Home from './components/home/Home';
+import Signup from './components/signup/Signup';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './App.css';
 
 function App() {
   return (
     <div >
+         <Home/>
+         <Signup/>
+         <Login/>
          <ContactUs />
     </div>
   );

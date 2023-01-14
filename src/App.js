6b9590949e250657a313de9f2406b17f7e159cf8
Rhoda Gachimu <gachimurhoda@gmail.com>
2023-01-15 02:38:48 +0300

    new updates

--- src/App.js
+++ src/App.js
@@ -2,25 +2,30 @@
 // import Appointment from './components/appointment/Appointment';
 // import Newsletter from './components/newsletter/Newsletter';
 import './App.css';
-// import NavBar from './components/navbar/NavBar'
-import Main from './components/Main'
+import NavBar from './components/navbar/NavBar'
+import Main from './components/mainpage/Main';
 import Footer from './components/footer/Footer'
 import Appointment from './components/appointment/Appointment';
-import { Route, Routes } from 'react-router-dom';
+import About from './components/about/About';
+import ContactUs from './components/contact-us/ContactUs';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-    {/* <NavBar /> */}
-
-    <div className='All_Content'>
+    <div className='app-container'>
+     
+      <NavBar />
+      {/* <Main /> */}
       <Routes>
-        <Route path='/' element={<Main />} />
-        <Route path='/appointment' element={<Appointment className='scroll-to-top' />} />
+      {/* <Main />  */}
+      <Route path='home' element={<Main/>} />
+        <Route path='about' element={<About />} />
+      <Route path='contact_us' element={<ContactUs />} />
+      <Route path='appointments' element={<Appointment />} />
       </Routes>
+      
       <Footer />
     </div>
-    </>
   );
 }
 
--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,89 +1,90 @@
 import React from 'react'
-// import Home from '../home-page/Home'
-// import hammer from '../../images/hammer.png'
+import Home from '../home-page/Home'
 import { Link } from 'react-router-dom'
+import Service from '../service/Service'
+// import hammer from '../../images/hammer.png'
 import './navbar.css'
 
 function NavBar() {
+  const menu = () => {
+    const menu = document.querySelector('.menu')
+    const navLinks = document.querySelector('.nav_links')
+    navLinks.classList.toggle('show')
+    menu.classList.toggle('active')
+  }
 
-    const menu = () => {
-        const menu = document.querySelector('.menu')
-        const navLinks = document.querySelector('.nav_links')
-        navLinks.classList.toggle('show')
-        menu.classList.toggle('active')
-    }
+  const link = () => {
+    const menu = document.querySelector('.menu')
+    const navLinks = document.querySelector('.nav_links')
+    navLinks.classList.remove('show')
+    menu.classList.remove('active')
+  }
 
-    const link = () => {
-        const menu = document.querySelector('.menu')
-        const navLinks = document.querySelector('.nav_links')
-        navLinks.classList.remove('show')
-        menu.classList.remove('active')
-    }
 
-    // ==== Links ScrollInteoView =====
-    const homeSection = () => {
-        const home = document.querySelector('.home_page')
-        home.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'start'})
-    }
-
-    const aboutSection = () => {
-        const about = document.querySelector('.about_section')
-        about.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'start'})
-    }
-
-    const serviceSection = () => {
-        const service = document.querySelector('.service-section')
-        service.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'start'})
-    }
-
-    const casesSection = () => {
-        const cases = document.querySelector('.cases-section')
-        cases.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'start'})
-    }
-
-    // Check The Url
-
-    function appointmentLink() {
-        window.scrollTo(0, 0)
-    }
-
+  const service = () => {
+    const about = document.querySelector('.service-section')
+    about.scrollIntoView({ behavior: 'smooth', block: 'end' })
+  }
 
   return (
     <nav>
-        {/* ==== Right Nav ==== */}
-        <div className='left_nav' >
-                <span onClick={() => {homeSection();}}>
-                    <Link to='/'>TOP LAWYERS</Link>
-                </span>
-            <hr/>
-        </div>
+      {/* ==== Right Nav ==== */}
+      <div className="left_nav">
+        <span>TOP LAWYERS</span>
+        <hr />
+      </div>
 
-        {/* ==== Middle Nav === */}
-        <div className='middle_nav'>
-            <div className='nav_links'>
-                <ul>
-                    <li onClick={() => {link(); homeSection() }}><Link to='/'>Home</Link></li>
-                    <li onClick={() => {link(); aboutSection() }}><Link to='/'>About</Link></li>
-                    <li onClick={() => {link(); serviceSection() }}><Link to='/'>Service</Link></li>
-                    <li onClick={() => {link(); casesSection() }}><Link to='/'>Top-Cases</Link></li>
-                </ul>
-            </div>
-        </div>
-        
-        {/* ==== Right Nav ==== */}
-        <div className='right_nav'>
-            <Link to='/appointment' onClick={appointmentLink} >
-                <div id='nav_appiontement'>
-                    APPOINTEMENT
-                </div>
+      {/* ==== Middle Nav === */}
+      <div className="middle_nav">
+        <div className="nav_links">
+          <ul>
+            <li>
+            <Link className="link" to="/home">
+              Home
             </Link>
-            {/* <div id='nav_member'>BECAME A MEMBER</div> */}
-            <div className='menu' onClick={menu}>
-                <span className='bar'></span>
-                <span className='bar'></span>
-                <span className='bar'></span>
-            </div>
+            </li>
+            <li>
+              <Link className="link" to="/about">
+              About
+            </Link>
+            </li>
+            <li>
+              <Link className="link" to="/contact_us">
+                Contact us
+              </Link>
+            </li>
+            <li
+              onClick={() => {
+                link()
+                service()
+              }}
+            >
+              Service
+            </li>
+            <li
+              onClick={() => {
+                link()
+              }}
+            >
+              Top-Cases
+            </li>
+          </ul>
         </div>
+      </div>
+
+      {/* ==== Right Nav ==== */}
+      <div className="right_nav">
+        <div id="nav_appiontement">
+            <Link className="link" to="/appointments">
+              Appointment
+            </Link></div>
+        <div id="nav_member">Login</div>
+        <div className="menu" onClick={menu}>
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>
+      </div>
     </nav>
   )
 }

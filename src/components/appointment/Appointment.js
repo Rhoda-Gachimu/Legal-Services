e2d873d9fcc1d88c4ca338d67615f195d6a8376b
Stephen-Njuguna-Njambi-Moringa <stepehen.jambi@student.moringaschool.com>
2023-01-15 21:52:35 +0300

    recreated the form

--- src/components/appointment/Appointment.js
+++ src/components/appointment/Appointment.js
@@ -1,69 +1,57 @@
 import React from 'react'
 import './appointment.css'
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 function Appointment() {
   return (
     <>
-        {/* example 3 */}
-        <div className='container'>
-            <h1 className='form-title'>Book Appointment</h1>
-            <form action='container-form'>
-                <div className='main-user-info'>
-                    <div className='user-input-info'>
-                        <label for="firstname">First Name</label>
-                        <input type="input-text" id="firstname" name="firstname" placeholder="Enter first name"></input>
-                    </div>
-                </div>
-                <div className='main-user-info'>
-                    <div className='user-input-info'>
-                        <label for="secondname">Second Name</label>
-                        <input type="input-text" id="secondname" name="secondname" placeholder="Enter second name"></input>
-                    </div>
-                </div>
-                <div className='main-user-info'>
-                    <div className='user-input-info'>
-                        <label for="useremail">Email</label>
-                        <input type="input-text" id="useremail" name="useremail" placeholder="Enter email"></input>
-                    </div>
-                </div>
-                <div className='main-user-info'>
-                    <div className='user-input-info'>
-                        <label for="user-contact">Phone no.</label>
-                        <input type="input-text" id="user-contact" name="user-contact" placeholder="Enter contact"></input>
-                    </div>
-                </div>
-                <div className='main-user-info'>
-                    <div className='user-input-info'>
-                        <label for="casetype">Case Type</label>
-                        <input type="texinput-text" id="placeholder-text" name="casetype" placeholder="Select casetype"></input>
-                    </div>
-                </div>
-                <div className='main-user-info'>
-                    <div className='user-input-info'>
-                        <label for="Appointment-date">Appointment Date</label>
-                        <input type="input-text" id="Appointment-date" name="Appointment-date" placeholder="Select Date"></input>
-                    </div>
-                </div>
-                <div className='gender-detail-box'>
-                    <span className='gender-detail-'></span>
-                    <div className='gender-type'>
+         {/* 2nd form  */}
+            <div class="main">
+                <div className="appointment-form">
+                    <h2 className='header-h2'>Book Appointment</h2>
+                    <form id='appointment' method='post'>
+                        <label >First name:</label><br></br>
+                        <input type="text" name="firstname" id='name' placeholder="Enter first name"></input><br></br>
+
+                        <label >Second name:</label><br></br>
+                        <input type="text" name="secondname" id='name' placeholder="Enter second name"></input><br></br>
+
+                        <label  >Email:</label><br></br>
+                        <input type="email" email="secondname" id='name' placeholder="Enter email adress"></input><br></br>
+
+                        <label className='genderlable'>Gender:</label><br></br>
+                        &nbsp;&nbsp;&nbsp;
                         <input type="radio" name="Gender" id='male' />
-                        <label for="male">Male</label> 
-                        <input type="radio" name="Gender" id='female' />
-                        <label for="female">Female</label> 
-                    </div>
+                        &nbsp;
+                        <span id='male'>Male</span>
+                        &nbsp;&nbsp;&nbsp;&nbsp;
+
+                        <input type="radio" name="Gender" id='femalemale' />
+                        &nbsp;
+                        <span id='female'>Female</span><br></br> 
+
+                        <label>Dispite Type:</label> <br></br>
+                        &nbsp;&nbsp;&nbsp;
+
+                        <input type="checkbox" name="disputetype" id='Criminal' />
+                        &nbsp;
+                        <span id='Criminal'>Criminal</span>
+                        &nbsp;&nbsp;&nbsp;&nbsp;
+                        
+
+                        <input type="checkbox" name="disputetype" id='Criminal' />
+                        &nbsp;
+                        <span id='Land'>Land</span>
+                        &nbsp;&nbsp;&nbsp;&nbsp;
+
+                        <input type="checkbox" name="disputetype" id='Land' />
+                        &nbsp;
+                        <span id='Family'>Family</span><br></br><br></br>
+
+                        <input type="submit" value="submit" name='submit' id="submit" /> 
+                    </form>
                 </div>
-
-                <div class="form-submit-btn">
-                    <inport type="submit" value="Book"></inport>
-                </div>
-                <input type="submit" value="Submit" />
-
-                <button type='submit'>Submit</button>
-
-            </form>
-        </div>
+            </div>
     </>
   )
 }

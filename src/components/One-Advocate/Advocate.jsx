d2094f09d44aa7b4af0bb100618a9f7c5b2a0b32
Ronaldkubasu <ronald.kubasu@student.moringaschool.com>
2023-01-15 10:43:26 +0300

    second commit

--- src/components/One-Advocate/Advocate.jsx
+++ src/components/One-Advocate/Advocate.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import './page.css'
+import './advocate.css'
 
 export default function Page() {
   return (
@@ -59,4 +59,5 @@ export default function Page() {
 
   )
 }
+
 
